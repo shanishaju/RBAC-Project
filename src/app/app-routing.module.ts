d60daf59c4be0b101ab08b16902738f45fa054a5
shanishaju <shanishaju84210@gmail.com>
2024-11-25 00:59:03 +0530

    Frontend design: Components created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'dashboard', component:DashboardComponent},
+  {path:'employee', component:EmployeeComponent},
+
+  {path:'add-employee', component:AddComponent},
+  {path:'edit/:id', component:EditComponent},
+  {path:'**', component:PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
