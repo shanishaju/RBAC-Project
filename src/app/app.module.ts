975deec2f442badf860cb4d0f798e87e0a601567
shanishaju <shanishaju84210@gmail.com>
2024-11-25 14:26:08 +0530

    design part completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { AddComponent } from './add/add.component';
 import { EditComponent } from './edit/edit.component';
 import { HeaderComponent } from './header/header.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
@@ -21,10 +26,16 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
     EditComponent,
     HeaderComponent,
     PagenotfoundComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule, 
+    MatDatepickerModule,
+    MatNativeDateModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  selected:Date= new Date()
+
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions={}; 
+  constructor(){
+    this.chartOptions ={
+      chart: {
+          type: 'pie'
+      },
+      title: {
+          text: 'Project Completion Report'
+      },
+      tooltip: {
+          valueSuffix: '%'
+      },
+      
+      plotOptions: {
+          series: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: [{
+                  enabled: true,
+                  distance: 20
+              }, {
+                  enabled: true,
+                  distance: -40,
+                  format: '{point.percentage:.1f}%',
+                  style: {
+                      fontSize: '1.2em',
+                      textOutline: 'none',
+                      opacity: 0.7
+                  },
+                  filter: {
+                      operator: '>',
+                      property: 'percentage',
+                      value: 10
+                  }
+              }]
+          }
+      },
+      credits:{
+        enabled:false
+    },
+      series: [
+          {
+              name: 'Project',
+              colorByPoint: true,
+              data: [
+                  {
+                      name: 'safari',
+                      y: 55.02
+                  },
+                  {
+                      name: 'chrome',
+                      sliced: true,
+                      selected: true,
+                      y: 26.71 
+                  },
+                  {
+                      name: 'firefox',
+                      y: 1.09
+                  },
+                  {
+                      name: 'edge',
+                      y: 15.5
+                  },
+                  {
+                      name: 'opera',
+                      y: 1.68
+                  }
+              ]
+          }
+      ]
+  }
+  }
 
 }
